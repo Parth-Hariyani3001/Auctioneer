c809c6f51e403bf3c1f06a8a7fe089683af34df3
Parth Hariyani <115411164+Parth-Hariyani3001@users.noreply.github.com>
2023-04-08 18:10:34 +0530

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,12 +5,12 @@ import 'firebase/firestore';
 
 // Use your own configs!
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyCTiT6TfCiI3GZpBcW4_YddDr0I2bkaw78",
-  authDomain: "auctioneer2-ce00c.firebaseapp.com",
-  projectId: "auctioneer2-ce00c",
-  storageBucket: "auctioneer2-ce00c.appspot.com",
-  messagingSenderId: "461722241242",
-  appId: "1:461722241242:web:f0ea64fa9bbed454573de3"
+  apiKey: "AIzaSyBv0VZ9zZ1rSBS2HYf0UZH-ciL7qqLH6gY",
+  authDomain: "auctioneer-93ca2.firebaseapp.com",
+  projectId: "auctioneer-93ca2",
+  storageBucket: "auctioneer-93ca2.appspot.com",
+  messagingSenderId: "53549720238",
+  appId: "1:53549720238:web:67a3265cc2f7ba5177c33f"
 });
 
 export const timestamp = firebase.firestore.FieldValue.serverTimestamp;
